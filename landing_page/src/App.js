7ca4d298320a98f5758a9259a6133e4179b35f3f
UttamMehta <binobajichiku@gmail.com>
2023-03-29 00:22:25 +0530

    first page

--- landing_page/src/App.js
+++ landing_page/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Component/Navbar";
+import Middlepages_01 from "./Component/Middlepages_01";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Middlepages_01 />
     </div>
   );
 }
--- landing_page/src/Component/middlepages_01.jsx
+++ landing_page/src/Component/middlepages_01.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import styles from "./"
+import styles from "./Middlepages_01.module.css"
 
-function middlepages_01(props) {
+function Middlepages_01(props) {
     return (
-        <div>
+        <div className={styles.mains}>
             <div>
             WELCOME TO CREATIVE PROXIES
             </div>
-            
+
+            <div>
+            <h1>
+            We are the Fastest and the Most
+             Reliable Proxy Service since 2020</h1>
+            </div>
+
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In luctus augue sit amet mollis molestie.</p>
+            </div>
+
+            <div>
+                <button>Get Started</button>
+            </div>
 
         </div>
     );
 }
 
-export default middlepages_01;
+export default Middlepages_01;
