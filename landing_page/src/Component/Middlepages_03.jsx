1731b0d6cd505ce1b3b679c0794b6b808429eeb1
UttamMehta <binobajichiku@gmail.com>
2023-03-29 20:03:52 +0530

    third middle page started

---
+++ landing_page/src/Component/Middlepages_03.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Middlepages_03(props) {
+    return (
+        <div>
+            
+        <div>
+            <h3>You choose, we <span>Deliver!</span></h3>
+        </div>
+
+        </div>
+    );
+}
+
+export default Middlepages_03;
