6b2bc9e5bf8c521f1dbf7b1ff030c06a618c278c
UttamMehta <binobajichiku@gmail.com>
2023-03-29 18:07:46 +0530

    middlepageone

--- landing_page/src/Component/middlepages_01.jsx
+++ landing_page/src/Component/middlepages_01.jsx
@@ -1,27 +1,59 @@
 import React from 'react';
+import Img_Content from '../pages/Img_Content';
 import styles from "./Middlepages_01.module.css"
 
 function Middlepages_01(props) {
     return (
         <div className={styles.mains}>
-            <div>
+            <div className={styles.page01_FirstDiv}>
             WELCOME TO CREATIVE PROXIES
             </div>
 
             <div>
             <h1>
-            We are the Fastest and the Most
-             Reliable Proxy Service since 2020</h1>
+            We are the <span className={styles.blue}>Fastest</span> and the <span className={styles.green}>Most
+             Reliable</span> Proxy Service since 2020</h1>
             </div>
 
-            <div>
+            <div className={styles.pages01_ThirdDiv}>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In luctus augue sit amet mollis molestie.</p>
             </div>
 
-            <div>
+            <div className={styles.pages01_FourthDiv}>
                 <button>Get Started</button>
             </div>
+        <div className={styles.pages01_FifthDiv}>
+        <div>
+    <span class="material-symbols-outlined">
+star_rate
+</span>
+<span class="material-symbols-outlined">
+star_rate
+</span>
+<span class="material-symbols-outlined">
+star_rate
+</span>
+<span class="material-symbols-outlined">
+star_rate
+</span>
+<span class="material-symbols-outlined">
+star_rate
+</span>
 
+        </div>
+        <div className={styles.page01_SixthDiv}>
+     <b> <span>  Rated 4.5 on</span>
+        <span class="material-symbols-outlined" style={{color:"green"}}>
+star_rate
+</span>
+<span>Trustpilot</span></b>
+        </div>
+       
+        </div>
+
+        <div className={styles.pages01_SeventhDiv}>
+        <Img_Content color={"green"}/>
+        </div>
         </div>
     );
 }
---
+++ landing_page/src/pages/Img_Content.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Img_Content(props) {
+    return (
+        <div>
+            <div>
+            <span class="material-symbols-sharp" style={{fill:props.color}}>
+rectangle
+</span></div>
+<div>
+    <span>Lorem Ipsum
+Dolor Sit Amet Top</span>
+</div>
+
+        </div>
+    );
+}
+
+export default Img_Content;
